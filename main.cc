64f7d11d0947dd539b2d977abb5240dea60c20f8
andrey <derzhavin.andrej@yandex.ru>
2021-03-29 20:24:02 +0300

    cin good added

--- main.cc
+++ main.cc
@@ -6,11 +6,23 @@ int main()
   size_t vec_size{};
 
   std::cin >> vec_size;
+  if (!std::cin.good())
+  {
+    std::cout << "Invalid character at stdin" << std::endl;
+    return -1;
+  }
 
   vec.resize(vec_size);
 
   for (auto &el : vec)
+  {
     std::cin >> el;
+    if (!std::cin.good())
+    {
+      std::cout << "Invalid character at stdin" << std::endl;
+      return -1;
+    }
+  }
 
   BTS::bsort(vec);
 
