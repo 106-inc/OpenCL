fa2d29993bdca082d165f34ec9dbb14ff1733a18
uslsteen <shurygin0171@gmail.com>
2021-04-06 02:17:42 +0300

    added #if 0 #end if

--- main.cc
+++ main.cc
@@ -27,21 +27,22 @@ int main()
 
     std::vector<int> comp_vec{vec};
 
+#if 0
     Time::Timer timer; 
     std::sort(comp_vec.begin(), comp_vec.end());
     std::cout << "std::sort(...) time: " << timer.elapsed() << " microseconds\n";
+#endif
 
 
     try
     {
         BTS::bsort(vec, BTS::Dir::INCR);
         
-        /*
         for (size_t i = 0; i < vec_size; ++i)
             std::cout << vec[i] << " ";
 
         std::cout << std::endl;
-        */
+        
     }
     catch (std::exception& err)
     {
@@ -49,7 +50,7 @@ int main()
         return -1;
     }
 
-
+#if 0
     std::cout << std::endl;
 
     if (vec == comp_vec)
@@ -62,6 +63,7 @@ int main()
         std::cout << "Sortion wasn't succesful!\n";
         return -1;
     }
+#endif
 
 
 }
